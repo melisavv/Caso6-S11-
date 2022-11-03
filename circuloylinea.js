fb42ebe95d180a9ac9d8fe38bdd42c515168ebb9
Melisa Vicente V <116923441+melisavv@users.noreply.github.com>
2022-11-02 21:14:47 -0500

    Update circuloylinea.js

--- circuloylinea.js
+++ circuloylinea.js
@@ -1,4 +1,4 @@
-//Mostrar un circulos desplazandose de arriba hacia abajo y una linea horizontal que choque con el circulo//
+//Mostrar un círculo desplazándose de arriba hacia abajo y una línea horizontal que choque con el circulo//
 for(var i=1;i<=1;i++){
     document.body.innerHTML+="<marquee behavior='alternate' height='600' SCROLLAMOUNT=20 direction='down'><div id='circulo'></marquee>"
     document.body.innerHTML+="<marquee behavior='alternate' height='600' SCROLLAMOUNT=20><div id='barra'></marquee>"
